ca6a3c48f9db71190160f57b233a5c1e2a1c6f56
root <root@ip-172-31-31-58.us-west-2.compute.internal>
2024-09-19 16:32:58 +0000

    Added changes in the file

--- myjentask2.sh
+++ myjentask2.sh
@@ -2,3 +2,5 @@
 echo "Hello, Jenkins Build!"
 This file is an jenkins task 2 file!
 I am doing this as an DevOps learner.
+I AM EDITING THIS IN ORDER TO MAKE CHANGES.
+The changes are being added to make changes at git and jenkins file! 
