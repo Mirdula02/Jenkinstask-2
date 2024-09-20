102b8dc66c320bd1179c9ce1b432473f0c60980a
MirdulaSree <mirdulasreekn@gmail.com>
2024-09-20 12:25:01 +0530

    Update myjentask2.sh

--- myjentask2.sh
+++ myjentask2.sh
@@ -4,3 +4,5 @@ This file is an jenkins task 2 file!
 I am doing this as an DevOps learner.
 I AM EDITING THIS IN ORDER TO MAKE CHANGES.
 The changes are being added to make changes at git and jenkins file! 
+making changes to be added
+The task is to make comments deditted from git to jenkins
